90a1c5218afb65c1cc22933fad1d395dd429f3aa
Dante-Berth <bertinalexAI@protonmail.com>
2024-01-08 17:03:55 +0100

    [UPDATE] pointer

--- learning_cpp/exercices_from_claude_delannoy/chap_4/pointers.cpp
+++ learning_cpp/exercices_from_claude_delannoy/chap_4/pointers.cpp
@@ -11,5 +11,30 @@ int main(){
     adi++; // pointer on the next adress not anymore on the adress of "n"
     cout<<(m == *adi)<<endl;
     cout<<((adi-1) == (&n))<<endl;// cout<<(adi-1) == (&n)<<endl; does not work , parenthesis needed
+    const int ing = 4;
+    int t [ing] = {8,5,6,7}; // t is an adress linked to the first element (8)
+    cout<< t <<endl;
+    cout<< (t+1) <<endl;
+    cout<< *(t+1) <<endl;
+    cout<< (t+2) <<endl;
+    cout<< *(t+2) <<endl;
+    int a [3];
+    int ii, jj;
+    int * adt;
+    for (ii=0,jj=0;ii<3;ii++)
+    {
+        a[ii] = jj++ + ii; // variable ++  post incremental
+        cout<<"#########"<<endl;
+        cout<<"ii = "<<ii<<endl;
+        cout<<"jj = "<<jj<<endl;
+        cout<<a[ii]<<endl;
+    }
+    for (adt = a; adt<(a+3); adt++) 
+    {
+        cout << *adt<< " ";
+        cout << adt ; 
+        cout << "\n";
+    }
+    
     return 0;
 }
